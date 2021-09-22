a51af45a9411180da4f291b92ef188fbe64b3fee
Lucie Choi <luce.the.bright@gmail.com>
2021-09-22 18:07:18 +0900

    [FEAT] section 3: MemberService

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //스프링부트로 불러와야 readOnly 옵션 사용 가능
+@RequiredArgsConstructor    //final 인 필드만 생성자 주입. 생성자가 한개이므로 @Autowired 도 자동으로 됨.
+public class MemberService {
+
+    public final MemberRepository memberRepository;
+
+    /***
+     * 회원 가입
+     */
+    @Transactional  //영속성 컨텍스트.
+    public Long join(Member member) {
+        validateDuplicateMember(member);    //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+
+    /***
+     * 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
